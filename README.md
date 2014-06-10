22229a0c3bd3c3961b5f37f2f84f046a33b774f4
INF1949 <infriedman@comcast.net>
2014-06-10 17:14:17 -0500

    ticketee
    Rails 3 in Action application used as a learning exercise

---
+++ README.md
@@ -0,0 +1,5 @@
+ticketee
+========
+
+Rails 3 in Action application
+This is a Rails application presented as a learning vehicle in the referenced book.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ticketee::Application.config.secret_token = '365c152b336537330781f16acf8c0eeb9b21966af22e5e3b50f862efdfb7488646807e78879f0d51373e168d935e7e8d013752a813ee567a90e508565cbc2846'
